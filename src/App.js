e26fded89591bd8dd00cf0179862c710ed287b49
pcsishun <pcsishun@Gmail.com>
2021-07-27 17:38:22 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react'
+ 
 
+
+const FromInput = (props) => {
+   const [todo, setTodo] = useState("");
+   return(
+     <>
+      <form onSubmit= {(e)=>  {
+        e.preventDefault();
+        console.log(`Submit: ${todo}`);
+        props.onTodoSubmit(todo);
+        setTodo("");
+      }}>
+        <input
+          id="todo-input"
+          type="text"
+          onChange={(e)=> setTodo(e.currentTarget.value)}
+        />
+        <button >+</button>
+
+      </form>
+     </>
+   )
+}
+
+
+const Todo = (props) => {
+  console.log(props)
+  return(
+    <div style={{display: 'flex'}}>
+      <li>{props.createTodo}</li>
+      <button        
+        type="submit"
+        onClick={() => props.onFinish({ todo:props.createTodo, date:props.date })}
+        >{'\u2713'}</button>
+    </div>
+
+  )
+}
+ 
 function App() {
+
+  const [todos, setTodos] = useState([])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Todo</h1>
+      <ul>
+        {todos.map(todo =>  <Todo 
+                              createTodo={todo.title} 
+                              date={todo.date}
+                              onFinish={(e) => setTodos(todos.filter(todo => todo.date !== e.date))   }/> )}
+      </ul>
+
+      <FromInput onTodoSubmit={(todo) => {
+        console.log(`todo: ${todo}`);
+        setTodos([...todos, {date:Date.now(), title: todo}]) //  เอาตัวหน้า ตั้ง เเละเพิ่มตัวหลัง
+        
+        }}/>
     </div>
   );
 }
+
+
 
 export default App;
